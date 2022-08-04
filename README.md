8e70c5923c76d2bb49ea271d35da79ccba5c58e5
imiebaka <triple07even@gmail.com>
2022-08-04 11:42:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# ExpressRedis
+
+## Table of Contents
+
+- [About](#about)
+- [Usage](#usage)
+- [Contributing](../CONTRIBUTING.md)
+
+## About <a name = "about"></a>
+
+I had a wonderfull talk with Abas and Chika, which inspired this implementation. I had to create this project to show Redis application Redis application
+
+
+### Prerequisites
+Clone the Repo and make installations
+
+```
+git clone XXX
+```
+
+### Installing
+You'll need Node installed on your machine to get this to work
+
+
+```
+cd xxx
+npm install && npm start
+```
+[Lunch the app on your pc](http://localhost:3001) 
+
+
+## Usage <a name = "usage"></a>
+This show an app with a promise that takes 1 to 3 seconds to load.
+The timeout is cached and used the next time the app run, making it lightning fast
+
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+const port = 3001;
+
+const redis = require("redis");
+const client = redis.createClient();
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+client.connect();
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function greet(req, res) {
+  if (client.get("cacheKey")) {
+    const result = await client.get("cacheKey");
+    res.send(`Hello, I just waited ${JSON.parse(result)} ms`);
+  } else {
+    const between1and3seconds =
+      1000 + Math.floor(Math.random() * Math.floor(2000));
+    wait(between1and3seconds).then(() => {
+      client.set("cacheKey", between1and3seconds, { EX: 7 });
+      res.send(`Hello, I just waited ${between1and3seconds} ms`);
+    });
+  }
+}
+
+app.get("/", greet);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
